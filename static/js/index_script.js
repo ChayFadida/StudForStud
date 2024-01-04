102b662e4e937e7d05a061ca1bc7f77b5f90543e
Chay Fadida <122904042+ChayFadida@users.noreply.github.com>
2024-01-04 22:20:32 +0200

    Chay/UUID (#1)
    * fix js
    
    * added uuid

--- static/js/index_script.js
+++ static/js/index_script.js
@@ -1,13 +1,20 @@
-// Add an event listener for the "upload-link" element
-document.getElementById("upload-link").addEventListener("click", function () {
-    var uploadForm = document.getElementById("upload-form");
-    if (uploadForm.style.display === "none" || uploadForm.style.display === "") {
-        uploadForm.style.display = "block";
-    } else {
-        uploadForm.style.display = "none";
-    }
+$(document).ready(function () {
+    // Add a click event listener to all "Open Form" buttons
+    $('.open-form-btn').on('click', function () {
+        // Show the corresponding modal form
+        $('#formModal').modal('show');
+        
+        // Populate the form fields with the values from the clicked row
+        var row = $(this).closest('tr');
+        $('#edit-semester').val(row.data('semester'));
+        $('#edit-exam_type').val(row.data('exam-type'));
+        $('#edit-grade').val(row.data('grade'));
+        $('#edit-course_code').val(row.data('course-code'));
+        $('#edit-lecturer').val(row.data('lecturer'));
+        $('#edit-notes').val(row.data('notes'));
+        $('#hidden-file-name').val(row.data('file-name'));
+    });
 });
-
 $(document).ready(function () {
     // Add a click event listener to all "Open Form" buttons
     $('.open-form-btn').on('click', function () {
@@ -26,6 +33,37 @@ $(document).ready(function () {
     });
 });
 
+$(document).ready(function() {
+    // Add an event listener to the file input element
+    const fileInput = $("#file-upload");
+    const fileInputLabel = $(".file-upload-label");
+
+    fileInput.change(function () {
+        if (fileInput[0].files.length > 0) {
+            // A file has been selected, update the label text
+            fileInputLabel.text(`קובץ נבחר`);
+        } else {
+            // No file selected, revert to the original label text
+            fileInputLabel.text("בחר קובץ");
+        }
+    });
+});
+
+$(document).ready(function() {
+    $('#datatable').DataTable({
+        language: {
+            url: '//cdn.datatables.net/plug-ins/1.13.6/i18n/he.json'
+        },
+        fixedColumns: {
+            left: 1,
+            right: 1
+        },
+        paging: true,
+        scrollCollapse: true,
+        scrollX: true,
+        bInfo : false
+    });
+});
 // Form validation function
 function validateUploadForm() {
     var lecturerInput = document.getElementById("lecturer").value;
