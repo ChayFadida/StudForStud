e894a17f2bdc0a5b7d9941d16a58f525562d6903
Chay Fadida <122904042+ChayFadida@users.noreply.github.com>
2024-03-18 21:27:50 +0200

    prometheus (#6)
    added flask prometheus metrics

--- config/app_contex.py
+++ config/app_contex.py
@@ -7,3 +7,5 @@ data_handler = HandleData(file_client)
 backup_dir = os.getenv("BACKUP_DIR")
 backup_cloud_username = os.getenv("BACKUP_CLOUD_USERNAME")
 backup_cloud_password = os.getenv("BACKUP_CLOUD_PASSWORD")
+app_version = os.getenv("APP_VERSION") or "null"
+app_environment = os.getenv("APP_ENVIRONMENT") or "null"
